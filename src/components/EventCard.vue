4b0a13722472c57274870b3592c16adfd2312330
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-07-11 21:16:56 +0700

    6

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 // defineProps<{
 //   msg: string
 // }>()
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
@@ -13,7 +25,12 @@
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div> -->
-  <div class="event-class"></div>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -26,6 +43,6 @@
 }
 .event-card:card {
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2)
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
