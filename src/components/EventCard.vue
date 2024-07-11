eff0d298e0d4a2b6560a9f4e361d845d67845307
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-07-11 21:09:54 +0700

    5

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -13,30 +13,19 @@
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div> -->
-  <div class="greetings"></div>
+  <div class="event-class"></div>
 </template>
 
-<!-- <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
 }
-
-h3 {
-  font-size: 1.2rem;
+.event-card:card {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2)
 }
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style> -->
+</style>
