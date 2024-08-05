79f8157fd88bef2e034e8487c0d53aa483917494
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-06 02:53:24 +0700

    Lab3 3.3

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
-import EventDetailView from '@/views/EventDetailView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
+import EventEditView from '@/views/event/EditView.vue'
 import AboutView from '@/views/AboutView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 
@@ -12,15 +15,35 @@ const router = createRouter({
       name: 'event-list-view',
       component: EventListView,
       props: (route) => ({
-        limit: parseInt(route.query.limit?.toString() || '1'),
+        limit: parseInt(route.query.limit?.toString() || '2'),
         page: parseInt(route.query.page?.toString() || '1')
       })
     },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
-      props: true
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
     },
     {
       path: '/about',
