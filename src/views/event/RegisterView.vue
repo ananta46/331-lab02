59330e1031fa7590d80eacd24c0f1eceebb5f66a
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-06 02:59:24 +0700

    Lab3 4.2

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
   event: Event
@@ -8,9 +9,11 @@ const props = defineProps<{
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
 const register = () => {
   //
   //
+  router.push({ name: 'event-detail-view' })
 }
 </script>
 
