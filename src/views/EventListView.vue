b0a8003af45c00b249f81a5bf32f55ef8f6f0b04
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-05 22:28:23 +0700

    Lab3 2.4 +

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -3,11 +3,20 @@ import EventCard from '@/components/EventCard.vue'
 import EventOrg from '@/components/EventOrg.vue'
 
 import Event from '@/types/Event'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import EventService from '@/services/EventService'
 const events = ref<Event[]>(null)
+
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true
+  }
+})
+const page = computed(() => props.page)
+
 onMounted(() => {
-  EventService.getEvents()
+  EventService.getEvents(2, page.value)
     .then((response) => {
       // console.log(response.data)
       events.value = response.data
@@ -24,7 +33,7 @@ onMounted(() => {
   <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <EventOrg v-for="event in events" :key="event.id" :event="event" />
+    <!-- <EventOrg v-for="event in events" :key="event.id" :event="event" /> -->
   </div>
 </template>
 <style scope>
