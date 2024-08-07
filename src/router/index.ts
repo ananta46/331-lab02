ff1c02d8facde4cd08a54dd13870285862b7f96c
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-08 03:49:18 +0700

    Lab4 3.2

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,8 @@ import StudentListView from '@/views/StudentListView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import nProgress from 'nprogress'
+import EventService from '@/services/EventService'
+import { error } from 'console'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,9 +29,21 @@ const router = createRouter({
       component: EventLayoutView,
       props: true,
       beforeEnter: (to) =>{
-        // call api here
-      }
-      ,
+        const id  = parseInt(to.params.id as string)
+        return EventService.getEvent(id)
+        .then((response) => {
+          // need to setup the data for the event
+        }).catch((error) => {
+          if (error.response && error.response.status === 404){
+            return {
+              name: '404-resource-view',
+              params: { resource: 'event'}
+            }
+          }else{
+            return { name: 'network-error-view' }
+          }
+        })
+      },
       children: [
         {
           path: '',
