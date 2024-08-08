9e196f8c11b44347a3476ba8e96a06037eb4e5ff
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-08 23:36:19 +0700

    lab05 7 extend fontFamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,12 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
+        sp: '0 3px 12px 0 rgba(0, 0, 0, 0.2)'
+      },
+      fontFamily: {
+        sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif']
       }
-    },
+    }
   },
   plugins: []
 } satisfies Config
