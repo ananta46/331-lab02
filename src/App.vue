7d272ac20a87e83d72087b378c55f9008773ce24
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-09 01:58:05 +0700

    lab05 deploy with speedAnalytics#2

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from './stores/message'
 import { storeToRefs } from 'pinia'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 const store = useMessageStore()
 const { message } = storeToRefs(store)
+injectSpeedInsights()
 </script>
 
 <template>
-  <SpeedInsights />
   <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="message">
