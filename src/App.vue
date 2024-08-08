42c8740faee140fc5e1b4ae7f6f4f8469d6e42ef
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-08 23:48:09 +0700

    lab05 9.1 exact-active green

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,20 @@ const { message } = storeToRefs(store)
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink>
+        <nav class="py-6">
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'event-list-view' }"
+            >Event</RouterLink
+          >
           |
-          <RouterLink :to="{ name: 'student' }">Student</RouterLink>
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'student' }"
+            >Student</RouterLink
+          >
         </nav>
       </div>
     </header>
